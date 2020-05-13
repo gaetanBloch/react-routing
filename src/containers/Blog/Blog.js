4718496ffc782b4f727b4966cb0dee06effed3ba
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-13 16:45:12 +0200

    Rendered routes for the list of posts and creati ng a new post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
   render() {
@@ -15,7 +17,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" component={Posts} exact />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
